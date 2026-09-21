cb7b83535657bbaa9ca73614560103dc0dd42625
Project <harshwani890.com>
2026-09-22 00:02:03 +0530

    Configure GitHub Pages deployment via GitHub Actions

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   reactStrictMode: true,
   transpilePackages: ['three', 'react-icons'],
+  images: {
+    unoptimized: true,
+  },
+  basePath: process.env.NODE_ENV === 'production' ? '/Framermotion-tags' : '',
 };
 
 module.exports = nextConfig;
